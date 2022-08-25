f012b7cbafd2871f22af8d9b20bf41899b124b8c
Andrew Briley <andrewbriley01@gmail.com>
2022-08-25 16:09:51 +0200

    Add URLs

--- main.py
+++ main.py
@@ -7,21 +7,24 @@ res = requests.get(website)
 soup = BeautifulSoup(res.text, 'html.parser')
 companies = soup.select('.base-search-card__subtitle')
 jobs = soup.select('.base-search-card__title')
+links = soup.select('.base-card__full-link ')
 
 
-def create_job_list(jobs, companies):
+def create_job_list(jobs, companies, links):
     job_list = []
     for i, j in enumerate(jobs):
-        job_list.append([i, j.get_text().strip()])
+        job_list.append([i, j.get_text().strip(), i+1])
     for i, c in enumerate(companies):
         job_list[i][0] = c.get_text().strip()
+    for i, l in enumerate(links):
+        job_list[i][2] = l.get('href')
     return job_list
 
 
-create_job_list(jobs, companies)
+create_job_list(jobs, companies, links)
 
-header = ['Company Name', 'Job Title']
-data = create_job_list(jobs, companies)
+header = ['Company Name', 'Job Title',  'URL']
+data = create_job_list(jobs, companies, links)
 
 with open('jobs.csv', 'w', encoding='UTF8', newline='') as f:
     writer = csv.writer(f)
