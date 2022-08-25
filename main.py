0513cabc194ddcb510900f9ddb776230940bff74
Andrew Briley <andrewbriley01@gmail.com>
2022-08-25 15:54:01 +0200

    Update script to scrape jobs and company names and write information into a CSV file

--- main.py
+++ main.py
@@ -1,20 +1,30 @@
 import requests
 from bs4 import BeautifulSoup
-import pprint
+import csv
 
-root = 'https://www.linkedin.com/jobs/search?keywords=Junior%2BSoftware%2BDeveloper&location=Barcelona%2C%2BCatalonia%2C%2BSpain&geoId=105088894&distance=25&f_E=2&currentJobId=3212513876&position=2&pageNum=0'
-# for page in range(1, 10):
-website = f'{root}'
+website = 'https://www.linkedin.com/jobs/search?keywords=Junior%2BSoftware%2BDeveloper&location=Barcelona%2C%20Catalonia%2C%20Spain&geoId=&trk=public_jobs_jobs-search-bar_search-submit&position=1&pageNum=0'
 res = requests.get(website)
 soup = BeautifulSoup(res.text, 'html.parser')
+companies = soup.select('.base-search-card__subtitle')
 jobs = soup.select('.base-search-card__title')
 
 
-def get_jobs(jobs):
-    job_posts = []
-    for title in jobs:
-        job_posts.append(title.get_text().strip())
-    print(job_posts)
+def create_job_list(jobs, companies):
+    job_list = []
+    for i, j in enumerate(jobs):
+        job_list.append([i, j.get_text().strip()])
+    for i, c in enumerate(companies):
+        job_list[i][0] = c.get_text().strip()
+    return job_list
 
 
-get_jobs(jobs)
+create_job_list(jobs, companies)
+
+header = ['Company Name', 'Job Title']
+data = create_job_list(jobs, companies)
+
+with open('jobs.csv', 'w', encoding='UTF8', newline='') as f:
+    writer = csv.writer(f)
+
+    writer.writerow(header)
+    writer.writerows(data)
